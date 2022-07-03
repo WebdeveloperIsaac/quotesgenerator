970b24d94aa3ec230970458224fe7aa38eac5db1
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-07-04 02:50:50 +0530

    Simple CSS

--- src/App.js
+++ src/App.js
@@ -27,12 +27,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <div className="card">
-          <h1 className="heading">{this.state.advice}</h1>
-          <button className="button" onClick={this.fetchAdvice}>
-            <span>GIVE ME ADVICE!</span>
-          </button>
-        </div>
+          <div className="card">
+              <h1 className="heading">{this.state.advice}</h1>
+              <button className="button" onClick={this.fetchAdvice}>
+                <span>Generate</span>
+              </button>
+            </div>
       </div>
     );
   }
