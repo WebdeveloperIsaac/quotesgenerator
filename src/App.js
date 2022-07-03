1eee13763312659b583a2673674a9fa2e0f8ff6d
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-07-04 01:43:21 +0530

    Fetching from the API with Axiosusing State Mount to call Method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import './App.css';
+import axios from 'axios';
 
+import './app.css';
 
-function App() {
+class App extends React.Component {
+  state = {
+    advice: '',
+  }
 
-    state = { advise : ''};
+  componentDidMount() {
+    this.fetchAdvice();
+  }
 
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
 
-  return (
-    <div>
-        <h1>Hello World</h1>
-    </div>
-  )
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
