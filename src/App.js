378a34826e8f8fd7226acb3e6be8386bbe9bafed
webdeveloperisaac <isaacinfrastructure@gmail.com>
2022-10-04 20:51:32 +0530

    Configured Tailwind Removed ORiginal CSs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
-import './app.css';
+import './index.css';
 
 class App extends React.Component {
   state = {
@@ -27,16 +26,19 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <h1 className='head'>Motivation Quote Generator</h1>
-      <div className="app">
-          <div className="card">
-              <h1 className="heading">{this.state.advice}</h1>
-              <button className="button" onClick={this.fetchAdvice}>
-                <span>Generate Motivation</span>
+      <div className="flex flex-col h-screen justify-between bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-400">
+      <h1 className='bg-black text-white text-center p-3 text-lg mb-2'>Motivation Quote Generator</h1>
+          <div className=''>
+            <div className=''>
+              <h1 className="text-center text-4xl font-medium">{this.state.advice}</h1>
+            </div>
+            <div className='flex items-center justify-center mt-5 animate-pulse'>
+              <button className="text-white font-medium p-3 rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500" onClick={this.fetchAdvice}>Load Another
               </button>
             </div>
+            </div>
+      <h5 className='bg-purple-300 text-black-100 text-center py-2 flex-row'> <span>Made With Love By <span className='animate-bounce'>❤️--</span> </span><a href='https://webdevisaac.com' className='bg-sky-300 underline rounded p-0.5 px-1  '>WebDeveloper@Isaac </a>🌎</h5>
       </div>
-      <h5>Isaac@webdevelopment</h5>
       </>
     );
   }
